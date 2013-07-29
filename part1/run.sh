293c5cdae5547dfa7c0ddbbe4ce8ab9864c0b211
Mark Williams <markrwilliams@gmail.com>
2013-07-29 00:24:40 -0700

    remove CLOS version in favor of DSL

--- part1/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/sbcl --eval "(asdf:operate 'asdf:load-op 'intro-to-comp-part-1)"
--- part1/src/package.lisp
+++
@@ -1,4 +0,0 @@
-(defpackage :intro-to-comp-part-1
-  (:use :common-lisp :asdf))
-
-(in-package :intro-to-comp-part-1)
